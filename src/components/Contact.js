05d6d0e293b13bc48aa6b31d15809734ee412271
Srijan Saha <srijan1336@gmail.com>
2021-01-06 01:47:46 +0530

    Contact V.1.1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import Button from './Button';
 import Heading from './Heading';
 
 function Contact() {
     return (
-        <div>
-            <div className="contact d-flex justify-content-center">
+        <div className=" d-flex justify-content-center">
+            <div className="contact">
                 <Heading heading={"Contact"} version={"secHeadingsWhite"}/>
+                <form>
+                    <div>
+                        <label>Full Name</label> <br/>
+                        <input className="contact__input" type="text" placeholder="Enter your name"/> <br/>
+                    </div>
+                    <div>
+                        <label>Email Address</label> <br/>
+                        <input className="contact__input" type="email" placeholder="Enter your email address"/> <br/>
+                    </div>
+                    <div>
+                        <label>Mobile Number</label> <br/>
+                        <input className="contact__input" type="number" placeholder="Enter your mobile number"/> <br/>
+                    </div>
+                    <div>
+                        <label>Message</label> <br/>
+                        <textarea className="contact__input" placeholder="What's in your mind?" rows="4" cols="50"/>
+                    </div>
+                    <Button text={"Submit"} version={"outlineButton"}/>
+                </form>
             </div>
         </div>
     )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,9 +17,6 @@ function Footer() {
                 <div className="col d-flex justify-content-start align-items-center"><Link to="/"><LazyLoadImage style={{margin: "85px"}} src={Icon}/></Link></div>
                 <div className="col d-flex justify-content-center flex-column">
                     <div className="d-flex justify-content-center">
-                        {/* <a href="https://www.linkedin.com/in/srijan-saha/" className="footer__icons"><i class="fab fa-facebook icon-last icons"></i></a>
-                        <a href="https://www.linkedin.com/in/srijan-saha/" className="footer__icons"><i class="fab fa-linkedin icon-last icons"></i></a>
-                        <a href="https://www.instagram.com/srijansahaa/" className="footer__icons"><i class="fab fa-instagram icon-last icons"></i></a> */}
                         <a href="https://www.facebook.com/srijan.saha3" className="footer__icons"><ReactSVG className="fab icon-last icons" src={Facebook}/></a>
                         <a href="https://www.linkedin.com/in/srijan-saha/" className="footer__icons"><ReactSVG className="fab icon-last icons" src={Linkedin}/></a>
                         <a href="https://www.instagram.com/srijansahaa/" className="footer__icons"><ReactSVG className="fab icon-last icons" src={Instagram}/></a>
@@ -28,9 +25,6 @@ function Footer() {
                     <a href="https://github.com/srijansahaa" className="footer__icons"><ReactSVG className="fab icon-last icons" src={Github}/></a>
                         <a href="mailto:srijan1336@gmail.com" className="footer__icons"><ReactSVG className="fab icon-last icons" src={Email}/></a>
                         <a href="https://www.behance.net/srijan-saha" className="footer__icons"><ReactSVG className="fab icon-last icons" src={Behance}/></a>
-                        {/* <a href="https://github.com/srijansahaa" className="footer__icons"><i class="fab fa-github icon-last icons"></i></a>
-                        <a href="mailto:srijan1336@gmail.com" className="footer__icons"><i class="fas fa-envelope icon-last icons"></i></a>
-                        <a href="https://medium.com/@srijansaha" className="footer__icons"><i class="fab fa-medium icon-last icons"></i></a> */}
                     </div>
                     <div className="d-flex justify-content-center footer__credits">Made with  &hearts;  by Srijan Saha !</div>
                 </div>
