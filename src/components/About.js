699a718cdc2ee9f3155c93dd50dad263c894b586
Srijan Saha <srijan1336@gmail.com>
2021-01-07 19:57:56 +0530

    Resposive & Text Animation

--- src/components/About.js
+++ src/components/About.js
@@ -7,31 +7,31 @@ import Me from '../assets/Me.jpg'
 function About() {
     return (
         <div id="about">
-            <div className="container d-flex justify-content-center">
+            <div className="d-flex justify-content-center">
                 <div><Heading heading={"ABOUT"} version={"secHeadings"}/></div>
             </div>
-            <div className="container">
-            <div className="row">
+            <div className="container aboutMain">
                 <div className="row">
-                    <div className="col-5 d-flex justify-content-center aboutLeft">
-                        <LazyLoadImage
-                            src={Me}
-                            className="me"
-                        />
-                    </div>
-                    <div className="col-7 d-flex flex-column aboutRight">
-                        <h2>I am Srijan</h2>
-                        <p>
-                            I'm a Computer Science Undergrad at SRMIST, Chennai<br/>
-                            currently in my second year. UI/UX Designer and a frontend web<br/>
-                            developer. I make beautiful designs and illustrations. I thrive on<br/>
-                            the challenge of leading interactions and visual design. I avoid<br/>
-                            vague meetings & hasty reactions.
-                        </p>
-                        <Button text={"Download Resume"} version={"longButton"}/>
+                    <div className="row">
+                        <div className="col-5 d-flex justify-content-center aboutLeft">
+                            <LazyLoadImage
+                                src={Me}
+                                className="me"
+                            />
+                        </div>
+                        <div className="col-7 d-flex flex-column aboutRight">
+                            <h2>I am Srijan</h2>
+                            <p>
+                                I'm a Computer Science Undergrad at SRMIST, Chennai
+                                currently in my second year. UI/UX Designer and a frontend web
+                                developer. I make beautiful designs and illustrations. I thrive on
+                                the challenge of leading interactions and visual design. I avoid
+                                vague meetings & hasty reactions.
+                            </p>
+                            <Button text={"Download Resume"} version={"longButton"}/>
+                        </div>
                     </div>
                 </div>
-            </div>
             </div>
         </div>
     );
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -10,17 +10,17 @@ import {
 
 function Landing() {
     return (
-        <div className="container">
+        <div className="container landingContainer">
             <div className="row align-items-center" style={{height: "710px"}}>
                 <div className="col landingLeft">
                     <h1>Hey</h1>
-                    <h1>I am <span>Srijan</span></h1>
-                    <h1>A Web Developer</h1>
+                    <h1>I am <span className="name">Srijan</span></h1>
+                    <h1>A <span className="textChange"></span></h1>
 
-                    <div className="d-flex justify-content-between" style={{marginTop: "30px"}}>
-                        <div><Link to="/work"><Button text={"Websites"} version={"blockButton"}/></Link></div>
-                        <div><Link to="/work"><Button text={"UI Designs"} version={"blockButton"}/></Link></div>
-                        <div><a href="https://drive.google.com/drive/folders/1teAx4DUSiy4rCmV1m6Hxd3iUxQFozg4X?usp=sharing"><Button text={"Posters"} version={"blockButton"}/></a></div>
+                    <div className="d-flex justify-content-between buttonContainer" style={{marginTop: "30px"}}>
+                        <div className="landingButtons"><Link to="/work"><Button text={"Websites"} version={"blockButton"}/></Link></div>
+                        <div className="landingButtons"><Link to="/work"><Button text={"UI Designs"} version={"blockButton"}/></Link></div>
+                        <div className="landingButtons"><a href="https://drive.google.com/drive/folders/1teAx4DUSiy4rCmV1m6Hxd3iUxQFozg4X?usp=sharing"><Button text={"Posters"} version={"blockButton"}/></a></div>
                     </div>
                 </div>
                 <div className="col d-flex justify-content-center">
