8af724a6db6256bc7ee51976eaede52654c6639f
Srijan Saha <srijan1336@gmail.com>
2021-07-17 17:43:28 +0530

    added scroll

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -43,51 +43,53 @@ const Admin = () => {
                 </h2>
                 <ReactSVG src={FirebaseLogo}/>
             </div>
-            <div className="container">
-                <div className="row admin__head">
-                    <div className="col-12 col-md-3 d-flex justify-content-center justify-content-md-start">
-                        <label className="admin__head--id">I.D</label>
-                    </div>
-                    <div className="col-2 col-md-1">
-                        <label>Name</label>
-                    </div>
-                    <div className="col-2 col-md-2">
-                        <label>Email</label>
-                    </div>
-                    <div className="col-2 col-md-2">
-                        <label>Number</label>
-                    </div>
-                    <div className="col-6 col-md-4">
-                        <label>Message</label>
+            <div className="admin__scroll mb-4">
+                <div className="container admin__scroll--head">
+                    <div className="row admin__head">
+                        <div className="col-3 d-flex justify-content-center justify-content-md-start">
+                            <label className="admin__head--id">I.D</label>
+                        </div>
+                        <div className="col-1">
+                            <label>Name</label>
+                        </div>
+                        <div className="col-2">
+                            <label>Email</label>
+                        </div>
+                        <div className="col-2">
+                            <label>Number</label>
+                        </div>
+                        <div className="col-4">
+                            <label>Message</label>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="container admin__content my-3">
-                {
-                    contact ?
-                    contact.map((c, index) => {
-                        return (
-                            <div className="row">
-                                <div className="col-12 col-md-3 d-flex justify-content-center justify-content-md-start">
-                                    <label className="admin__content--id" key={index}>{c.id}</label>
+                <div className="container admin__content my-3">
+                    {
+                        contact ?
+                        contact.map((c, index) => {
+                            return (
+                                <div className="row">
+                                    <div className="col-3 d-flex justify-content-center justify-content-md-start">
+                                        <label className="admin__content--id" key={index}>{c.id}</label>
+                                    </div>
+                                    <div className="col-1">
+                                        <label key={index}>{c.name}</label>
+                                    </div>
+                                    <div className="col-2">
+                                        <a href={`mailto:${c.email}`} key={index}>{c.email}</a>
+                                    </div>
+                                    <div className="col-2">
+                                        <a href={`tel:${c.number}`} key={index}>{c.number}</a>
+                                    </div>
+                                    <div className="col-4">
+                                        <p key={index}>{c.message}</p>
+                                    </div>
                                 </div>
-                                <div className="col-2 col-md-1">
-                                    <label key={index}>{c.name}</label>
-                                </div>
-                                <div className="col-2 col-md-2">
-                                    <a href={`mailto:${c.email}`} key={index}>{c.email}</a>
-                                </div>
-                                <div className="col-2 col-md-2">
-                                    <a href={`tel:${c.number}`} key={index}>{c.number}</a>
-                                </div>
-                                <div className="col-6 col-md-4">
-                                    <p key={index}>{c.message}</p>
-                                </div>
-                            </div>
-                        )
-                    }) :
-                    <label>No response!</label>
-                }
+                            )
+                        }) :
+                        <label>No response!</label>
+                    }
+                </div>
             </div>
             <div className=" d-flex justify-content-center">
                 <button onClick={openWeb}>Website</button>
